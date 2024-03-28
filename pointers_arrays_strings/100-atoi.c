9531f11e319b54034ddc7bc7d0094ebef390af8b
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-28 14:04:27 +0000

    Project pointers_arrays_strings-10: change when negation is applied to allow parsing of INT_MIN

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,6 +10,7 @@ int _atoi(char *s)
 	int result = 0;
 	int negative = 0;
 	int parsing_started = 0;
+	int to_add = 0;
 
 	while (*(s + index) != '\0')
 	{
@@ -17,7 +18,10 @@ int _atoi(char *s)
 
 		if (c >= '0' && c <= '9')
 		{
-			result = result * 10 + (c - '0');
+			to_add = c - '0';
+			if (negative % 2 == 1)
+				to_add *= -1;
+			result = result * 10 + to_add;
 			parsing_started = 1;
 		}
 		else if (parsing_started > 0)
@@ -27,9 +31,6 @@ int _atoi(char *s)
 
 		index++;
 	}
-
-	if (negative % 2 == 1)
-		result *= -1;
 
 	return (result);
 }
