3d4e5b354b77eaea9d322b026469173c9c14a4a6
Judgy53 <alexandre.gaubert94@orange.fr>
2024-04-03 12:25:42 +0000

    Project doubly_linked_lists-12: betty compliance

--- doubly_linked_lists/102-main.c
+++ doubly_linked_lists/102-main.c
@@ -1,60 +1,74 @@
 #include <stdio.h>
 
-int main (void)
+char isPalindrome(int);
+
+/**
+ * main - finds the largest 3 digits palindrome
+ *
+ * Return: always 0
+ */
+int main(void)
 {
-  int a = 999;
-  int b = 999;
-  int pal = 0;
+	int a = 999;
+	int b = 999;
+	int pal = 0;
 
-  int largest_a = 0;
-  int largest_b = 0;
-  int largest_pal = 0;
+	int l_a = 0;
+	int l_b = 0;
+	int l_pal = 0;
 
-  while (a >= 100)
-  {
-    b = 999;
-    while (b >= 100)
-    {
-      pal = a * b;
-      
-      if (pal <= largest_pal)
-        break;
+	FILE *f;
 
-      if (isPalindrome(pal) > 0) 
-      {
-        largest_pal = pal;
-        largest_a = a;
-        largest_b = b;
-      }
+	while (a >= 100)
+	{
+		b = 999;
+		while (b >= 100)
+		{
+			pal = a * b;
 
-      b--;
-    }
-    a--;
-  }
+			if (pal <= l_pal)
+				break;
 
-  printf("Largest Palindrome: %d * %d = %d\n", largest_a, largest_b, largest_pal);
-  
-  FILE* f = fopen("102-result", "w");
-  if (f != NULL)
-  {
-    fprintf(f, "%d", largest_pal);
-    fclose(f);
-    f = NULL;
-  }
+			if (isPalindrome(pal) > 0)
+			{
+				l_pal = pal;
+				l_a = a;
+				l_b = b;
+			}
+			b--;
+		}
+		a--;
+	}
 
-  return (0);
+	printf("Largest Palindrome: %d * %d = %d\n", l_a, l_b, l_pal);
+
+	f = fopen("102-result", "w");
+	if (f != NULL)
+	{
+		fprintf(f, "%d", l_pal);
+		fclose(f);
+		f = NULL;
+	}
+
+	return (0);
 }
 
-int isPalindrome (int number)
+/**
+ * isPalindrome - check if a number is a palindrome
+ * @number: number to check
+ *
+ * Return: 1 if number is palindrome, 0 otherwise
+ */
+char isPalindrome(int number)
 {
-  int reversed = 0;
-  int n = number;
+	int reversed = 0;
+	int n = number;
 
-  while (n != 0)
-  {
-    reversed = reversed * 10 + n % 10;
-    n /= 10;
-  }
+	while (n != 0)
+	{
+		reversed = reversed * 10 + n % 10;
+		n /= 10;
+	}
 
-  return (number == reversed);
+	return (number == reversed);
 }
